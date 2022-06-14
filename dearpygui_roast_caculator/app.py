ea58f48c430ddc8c17474d69742938f5b88c22a5
YUvindu C <chany975@hsd.k12.or.us>
2022-06-14 16:37:13 -0700

    adding primary widgets

--- dearpygui_roast_caculator/app.py
+++ dearpygui_roast_caculator/app.py
@@ -1,1 +1,22 @@
 # app.py - My DearPyGUI windowed app
+from cProfile import label
+import dearpygui.dearpygui as dpg
+
+dpg.create_context()
+dpg.create_viewport(title='Roast Caculator', width=600, height=300)
+
+with dpg.window(label="Roast Caculator", width=600, height=300 ):
+    dpg.add_text("Welcome to the Roast Caculator")
+    dpg.add_input_int(label="maximum value", width=100)
+    dpg.add_input_text(label="Action 1", width=200)
+    dpg.add_input_int(label="Action 1 value", width=100)
+    dpg.add_input_text(label="Action 2", width=200)
+    dpg.add_input_int(label="Action 2 value", width=100)
+    dpg.add_input_text(label="Action 3", width=200)
+    dpg.add_input_int(label="Action 3 value", width=100)
+    dpg.add_button(label="Calculate")
+    dpg.add_button(label="Clear")
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
